13232ec47b68a0eb08d13f99a5e3b0ef520facf4
AidanH567 <herstikaidan@gmail.com>
2024-10-14 11:15:07 +1100

    baseline code for displaying tracks.

--- displayTracks.js
+++ displayTracks.js
@@ -1,44 +1,23 @@
 // UI Module
 const UIController = (function () {
-  //object to hold references to html selectors
+  // Object to hold references to HTML selectors
   const DOMElements = {
-    selectGenre: "#select_genre",
-    selectPlaylist: "#select_playlist",
-    buttonSubmit: "#btn_submit",
-    divSongDetail: "#song-detail",
-    hfToken: "#hidden_token",
-    divSonglist: ".song-list",
+    buttonSubmit: "#btn_submit", // Keep if you have a submit button
+    divSongDetail: "#song-detail", // For displaying track details
+    divSonglist: ".song-list", // For displaying the list of tracks
   };
 
-  //public methods
+  // Public methods
   return {
-    //method to get input fields
+    // Method to get input fields if needed
     inputField() {
       return {
-        genre: document.querySelector(DOMElements.selectGenre),
-        playlist: document.querySelector(DOMElements.selectPlaylist),
         tracks: document.querySelector(DOMElements.divSonglist),
-        submit: document.querySelector(DOMElements.buttonSubmit),
         songDetail: document.querySelector(DOMElements.divSongDetail),
       };
     },
 
-    // need methods to create select list option
-    createGenre(text, value) {
-      const html = `<option value="${value}">${text}</option>`;
-      document
-        .querySelector(DOMElements.selectGenre)
-        .insertAdjacentHTML("beforeend", html);
-    },
-
-    createPlaylist(text, value) {
-      const html = `<option value="${value}">${text}</option>`;
-      document
-        .querySelector(DOMElements.selectPlaylist)
-        .insertAdjacentHTML("beforeend", html);
-    },
-
-    // need method to create a track list group item
+    // Method to create a track list group item
     createTrack(id, name) {
       const html = `<a href="#" class="list-group-item list-group-item-action list-group-item-light" id="${id}">${name}</a>`;
       document
@@ -46,10 +25,10 @@ const UIController = (function () {
         .insertAdjacentHTML("beforeend", html);
     },
 
-    // need method to create the song detail
+    // Method to create the song detail
     createTrackDetail(img, title, artist) {
       const detailDiv = document.querySelector(DOMElements.divSongDetail);
-      // any time user clicks a new song, we need to clear out the song detail div
+      // Clear the song detail div when a new song is clicked
       detailDiv.innerHTML = "";
 
       const html = `
@@ -57,10 +36,10 @@ const UIController = (function () {
                 <img src="${img}" alt="">        
             </div>
             <div class="row col-sm-12 px-0">
-                <label for="Genre" class="form-label col-sm-12">${title}:</label>
+                <label class="form-label col-sm-12">${title}:</label>
             </div>
             <div class="row col-sm-12 px-0">
-                <label for="artist" class="form-label col-sm-12">By ${artist}:</label>
+                <label class="form-label col-sm-12">By ${artist}:</label>
             </div> 
             `;
 
@@ -74,21 +53,6 @@ const UIController = (function () {
     resetTracks() {
       this.inputField().tracks.innerHTML = "";
       this.resetTrackDetail();
-    },
-
-    resetPlaylist() {
-      this.inputField().playlist.innerHTML = "";
-      this.resetTracks();
-    },
-
-    storeToken(value) {
-      document.querySelector(DOMElements.hfToken).value = value;
-    },
-
-    getStoredToken() {
-      return {
-        token: document.querySelector(DOMElements.hfToken).value,
-      };
     },
   };
 })();
