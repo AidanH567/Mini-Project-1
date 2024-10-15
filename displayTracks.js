7a307bf7111e612c1cd4e6b49d5425c6463be09a
AidanH567 <herstikaidan@gmail.com>
2024-10-15 14:42:01 +1100

    Changing the display track ui model to be used with my own ui

--- displayTracks.js
+++ displayTracks.js
@@ -1,10 +1,8 @@
-// UI Module
+// Display Track UI Model
 const UIController = (function () {
   // Object to hold references to HTML selectors
   const DOMElements = {
-    buttonSubmit: "#btn_submit", // Keep if you have a submit button
-    divSongDetail: "#song-detail", // For displaying track details
-    divSonglist: ".song-list", // For displaying the list of tracks
+    divSonglist: ".section-list ol", // Targeting the ordered list
   };
 
   // Public methods
@@ -13,46 +11,27 @@ const UIController = (function () {
     inputField() {
       return {
         tracks: document.querySelector(DOMElements.divSonglist),
-        songDetail: document.querySelector(DOMElements.divSongDetail),
       };
     },
 
-    // Method to create a track list group item
-    createTrack(id, name) {
-      const html = `<a href="#" class="list-group-item list-group-item-action list-group-item-light" id="${id}">${name}</a>`;
+    // Method to create a track list group item with details
+    createTrack(id, name, img, artist) {
+      const html = `
+        <li>
+          <h1>${id}</h1>
+          <article>
+            <div>${name}</div>
+            <div>By ${artist}</div>
+          </article>
+          <img src="${img}" alt="${name} image">
+        </li>`;
       document
         .querySelector(DOMElements.divSonglist)
         .insertAdjacentHTML("beforeend", html);
     },
 
-    // Method to create the song detail
-    createTrackDetail(img, title, artist) {
-      const detailDiv = document.querySelector(DOMElements.divSongDetail);
-      // Clear the song detail div when a new song is clicked
-      detailDiv.innerHTML = "";
-
-      const html = `
-            <div class="row col-sm-12 px-0">
-                <img src="${img}" alt="">        
-            </div>
-            <div class="row col-sm-12 px-0">
-                <label class="form-label col-sm-12">${title}:</label>
-            </div>
-            <div class="row col-sm-12 px-0">
-                <label class="form-label col-sm-12">By ${artist}:</label>
-            </div> 
-            `;
-
-      detailDiv.insertAdjacentHTML("beforeend", html);
-    },
-
-    resetTrackDetail() {
-      this.inputField().songDetail.innerHTML = "";
-    },
-
     resetTracks() {
       this.inputField().tracks.innerHTML = "";
-      this.resetTrackDetail();
     },
   };
 })();
